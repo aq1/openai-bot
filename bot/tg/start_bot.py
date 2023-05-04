687da9291583773545be4554479a0ddc02de8520
aq1 <aq1@aq1.ru>
2023-05-04 15:32:32 +0300

    Add notifications

--- bot/tg/start_bot.py
+++ bot/tg/start_bot.py
@@ -1,8 +1,25 @@
+import asyncio
+
 from django.conf import settings
 from telegram.ext import Application, ContextTypes
 
+from ..models import TelegramUser
+
 from .handlers import HANDLER_GROUPS
 from .handlers.l10n_context import L10nContext
+
+
+def notify(text: str):
+    async def func(application: Application):
+        users = TelegramUser.objects.filter(groups__name='Admin')
+        async with asyncio.TaskGroup() as tg:
+            async for user in users:
+                tg.create_task(application.bot.send_message(
+                    chat_id=user.id,
+                    text=text,
+                ))
+
+    return func
 
 
 def start_bot():
@@ -10,6 +27,10 @@ def start_bot():
 
     application = Application.builder().token(
         settings.TELEGRAM_TOKEN,
+    ).post_init(
+        notify('Initialized'),
+    ).post_stop(
+        notify('Stopped'),
     ).context_types(
         context_types,
     ).concurrent_updates(
