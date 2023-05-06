280fbf20aadcfaab02280167f638c5cbf9f51c1c
aq1 <aq1@aq1.ru>
2023-05-06 16:38:26 +0300

    Change scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,14 +6,16 @@ git pull
 source ".venv/bin/activate"
 pip install -U -q -r requirements.txt
 
-cp scripts/summary_bot.service /etc/systemd/system/
-cp scripts/summary_django.service /etc/systemd/system/
+cp scripts/openai_dalle_bot.service /etc/systemd/system/
+cp scripts/openai_summary_bot.service /etc/systemd/system/
+cp scripts/openai_django.service /etc/systemd/system/
 
 ./manage.py collectstatic --noinput
 ./manage.py compilemessages > /dev/null
 ./manage.py migrate
 
 systemctl daemon-reload
-systemctl restart summary_bot
-systemctl restart summary_django
+systemctl restart openai_dalle_bot
+systemctl restart openai_summary_bot
+systemctl restart openai_django
 echo "Finished"
